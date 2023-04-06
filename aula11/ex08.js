c3a5363f34d5b710b1a2b17f912d46e46db454b0
BRENO LOPES DE NOVAES <128421985+Brenolopesdenovaes@users.noreply.github.com>
2023-04-06 15:58:53 -0300

    .

---
+++ aula11/ex08.js
@@ -0,0 +1,6 @@
+var vel = 12
+console.log(`a velocidade do seu carrro é ${vel}km/h`)
+if(vel>60){
+    console.log('Voce ultrapassou a velocidade permitida será multado')
+}
+console.log(`Dirija sempre usando cinto de segurança`)
---
+++ aula11/ex09.js
@@ -0,0 +1,7 @@
+var país='Brasil'
+console.log(`vivendo em ${país}`)
+if(país == 'Brasil'){
+    console.log(' É brasileiro')
+}else{
+    console.log(' É estrangeiro')
+}
