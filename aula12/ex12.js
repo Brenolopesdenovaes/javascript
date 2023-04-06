0a7782175e06beb43012dfc0e4414f968c5dcb00
BRENO LOPES DE NOVAES <128421985+Brenolopesdenovaes@users.noreply.github.com>
2023-04-06 16:59:21 -0300

    .

---
+++ aula12/ex12.js
@@ -0,0 +1,8 @@
+var idade = 67
+if(idade < 16){
+    console.log (' n vota')
+}else if(idade <18 || idade > 65){
+     console.log('voto opcional')
+ }else {(idade>=18) 
+    console.log('obrigatorio')
+ }
