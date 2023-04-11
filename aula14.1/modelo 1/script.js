3c1434a08e8587bd6aeb01a17fd3a32193abbb1f
BRENO LOPES DE NOVAES <128421985+Brenolopesdenovaes@users.noreply.github.com>
2023-04-11 16:33:52 -0300

    .

---
+++ aula14.1/modelo 1/script.js
@@ -0,0 +1,27 @@
+function contar() {
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+    
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert('Erro')
+    } else {
+        res.innerHTML = 'Contando ...'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        
+        if (i < f) {
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        } else {
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1f3c1}`
+    }
+}
+
