8dcca6339fcde001a0725f5ca579cb9eced74323
BRENO LOPES DE NOVAES <128421985+Brenolopesdenovaes@users.noreply.github.com>
2023-04-11 13:40:22 -0300

    .

--- aula13/ex14/script.js
+++ aula13/ex14/script.js
@@ -1,1 +1,17 @@
-alert('olá')
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas`
+    if (hora>=0 && hora < 12){
+        img.src="imagens/manha.jpg"
+        document.body.style.background="blue"
+    }else if (hora>=12 && hora <= 18){
+        img.src="imagens/tarde.jpg"
+        document.body.style.background="orange"
+    }else{
+    img.src="imagens/noite.jpg"
+    document.body.style.background="black"
+}
+}
